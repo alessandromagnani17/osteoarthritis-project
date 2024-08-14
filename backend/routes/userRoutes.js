1df5477bb21f4606dcc7bbfffb0646b0241106de
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-08-14 14:01:30 +0200

    Set up Flask backend with Amazon Cognito integration

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/userController');
-
-// Definisci le rotte
-router.post('/register', UserController.createUser);
-
-module.exports = router;
