46f3f82ca7c72c14e83574e4bbad0b3f6ce0d697
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-08-14 14:10:55 +0200

    Add Vue.js frontend for user registration and authentication

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,23 +1,7 @@
-// vue.config.js
 module.exports = {
   devServer: {
-    port: 8080, // La stessa porta su cui gira il tuo server di sviluppo Vue.js
-    host: '0.0.0.0',
-    https: false,
-    allowedHosts: 'all',
-    client: {
-      webSocketURL: {
-        hostname: '0.0.0.0',
-        port: 8080,
-        protocol: 'ws',
-      },
-    },
-  },
-  configureWebpack: {
-    stats: {
-      all: false,
-      errors: true,
-      warnings: true
-    }
+    proxy: 'https://localhost:3000',
+    changeOrigin: true,
+    secure: false
   }
-};
+}
