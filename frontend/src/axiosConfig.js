41556fce3d8c85df5b72eb926a50082c09cf9640
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-09-19 15:55:54 +0200

    Fix axios configuration in axiosConfig.js to set baseURL and default headers

---
+++ frontend/src/axiosConfig.js
@@ -0,0 +1,10 @@
+// frontend/src/axiosConfig.js
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://13.60.212.129:5000', // Cambia con l'URL del tuo backend
+  timeout: 10000,
+  headers: {'Content-Type': 'application/json'}
+});
+
+export default instance;
