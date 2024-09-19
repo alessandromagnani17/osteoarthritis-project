120e33fcff4e4c74aa3f76084039cdc278733c53
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-09-19 16:41:07 +0200

    Create file .eslintrc.js

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    extends: [
+      'plugin:vue/vue3-essential',
+      'eslint:recommended',
+      '@vue/prettier'
+    ],
+    parserOptions: {
+      parser: 'babel-eslint'
+    },
+    rules: {
+      'prettier/prettier': ['error', { singleQuote: true, semi: false }],
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+  }
+  
