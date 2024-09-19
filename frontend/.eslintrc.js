c7270abe5cd8ae4e3874b8655e4bc5d6f0fbb67c
alessandromagnani <alessandro.magnani18@studio.unibo.it>
2024-09-19 17:16:43 +0200

    Modify .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,12 +13,18 @@ module.exports = {
       requireConfigFile: false,
       babelOptions: {
         presets: ['@babel/preset-env']
-      }
+      },
+      ecmaVersion: 2020,  // Specifica la versione ECMAScript
+      sourceType: 'module' // Assicura che il parser riconosca i moduli ES6
     },
+    plugins: [
+      'prettier'
+    ],
     rules: {
       'prettier/prettier': ['error', { singleQuote: true, semi: false }],
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }] // Aggiunge una regola per variabili non utilizzate
     }
   }
   
